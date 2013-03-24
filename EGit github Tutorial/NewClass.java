27c3fbaa49e48608e5088aa95cd2f156dd1e4032
Simon290 <simon@skaletons.de>
2013-03-24 22:20:10 +0100

    jay first commit

---
+++ EGit github Tutorial/NewClass.java
@@ -0,0 +1,16 @@
+
+public class NewClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// this ist a new Class and i hope it works
+		
+		int c = 4;
+		
+	}
+
+}
